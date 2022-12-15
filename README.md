0351e0b53495919c60cc0376ed8fa0f55d50db36
Emmanuel Rodríguez <rorem.94.developer@gmail.com>
2022-12-15 11:58:59 -0600

    feat: ✨ init application

---
+++ README.md
@@ -0,0 +1,14 @@
+<img src="./project/logo.svg" alt="Logo" width="80" height="80" />
+
+# Vizualla
+
+> Blogger Visual Platform
+
+## Development
+
+![NextJS](https://img.shields.io/badge/v13.X-ECEFF4?style=for-the-badge&logo=Next.js&logoColor=2E3440)
+
+To start developing components in this project you will use NextJS as core framework. For that you need to follow the steps below:
+
+1. Install the dependencies `pnpm install`.
+2. Launch Storybook process `pnpm dev`.
---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Vizualla platform</title>
+                <meta name="description" content="Vizualla platform" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+            </Head>
+            <main>
+                <h1>Hello world 🤙</h1>
+            </main>
+        </>
+    )
+}
